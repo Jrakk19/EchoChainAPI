62edee729210a44589cdb80fed6a18907d0f4207
Jack B <jackjblackwell@gmail.com>
2023-01-13 11:46:52 -0700

    Add Security JWT Authentication
    This adds authentication to api calls via JWT tokens provided by cognito

---
+++ src/main/java/com/echochain/EchoChainAPI/configurations/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.echochain.EchoChainAPI.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                .and()
+                .authorizeRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry.anyRequest().authenticated())
+                .oauth2ResourceServer().jwt();
+    }
+}
+
